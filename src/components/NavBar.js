c433d0111335bc214864db5823e8ed6241aa2995
Connor Lagana <connor.lagana@gmail.com>
2019-12-19 11:25:18 -0500

    fixed nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,6 @@ const NavBar = () => {
     <div className="nav">
       <ul>
         <li>Home</li>
-        <li>News</li>
-        <li>Contact</li>
-        <li>About</li>
       </ul >
     </div >
   )
