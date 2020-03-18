0574a288b9cafd5374f47d681c759078cf9e5587
Connor Lagana <connor.lagana@gmail.com>
2020-03-18 19:53:34 -0400

    changed nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="nav">
       <ul>
         <Link to={`/`}>
-          <li>Home</li>
+          <li id="listCom">Home</li>
         </Link>
-        <a href='https://www.youtube.com/watch?v=WIrWyr3HgXI'>Are you silly?</a>
-      </ul >
-    </div >
-  )
-}
+        <a id="listCom" href="https://www.youtube.com/watch?v=WIrWyr3HgXI">
+          Are you silly?
+        </a>
+      </ul>
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
